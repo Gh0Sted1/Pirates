b11924f8fa7294200176ad5bdf60566b0bfaaae4
Gh0Sted1 <71710553+Gh0Sted1@users.noreply.github.com>
2020-11-03 14:16:22 -0500

    e

--- PiratesStep1.java
+++ PiratesStep1.java
@@ -1,5 +1,5 @@
 // Import the GUI libraries
-import javax.swing.*;
+import javax.swing.*; 
 import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -37,12 +37,60 @@ public class PiratesStep1 {
 		JFrame frame = new JFrame ("Pirates");
 		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		frame.setResizable (false);
+		
+		
 
 		// The panel that will hold the components in the frame.
 		JPanel contentPane = new JPanel ();
+		contentPane.setPreferredSize(new Dimension(950, 400));
+		contentPane.setLayout(new BorderLayout());
 
-		// TODO: Add the components to the view
+		JPanel content = new JPanel ();
+		content.setLayout(new BoxLayout(content, BoxLayout.PAGE_AXIS));
+		contentPane.add(content, BorderLayout.EAST);
 		
+		
+		JLabel scoreTitle = new JLabel ("Score:");
+		scoreTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
+		content.add(scoreTitle);
+		
+		JLabel scoreNumber = new JLabel ("0");
+		scoreNumber.setAlignmentX(Component.CENTER_ALIGNMENT);
+		content.add(scoreNumber);
+		
+		JLabel actions = new JLabel ("Actions");
+		actions.setAlignmentX(Component.CENTER_ALIGNMENT);
+		content.add(actions);
+		
+		JButton quitButton = new JButton ("Quit");
+		quitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		content.add(quitButton);
+		
+		JButton startButton = new JButton ("New Game");
+		startButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		content.add(startButton);
+		
+		JButton musicButton = new JButton ("Music");
+		musicButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		content.add(musicButton);
+		
+		//Add the map
+		JLayeredPane gamePanel = new JLayeredPane ();
+		contentPane.add(gamePanel);
+		gamePanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		JLabel gameImage = new JLabel(new ImageIcon("resources/world-map-animals.jpg"));
+		gameImage.setSize(775, 400);
+		gamePanel.add(gameImage);
+		
+		JLabel shipImage = createScaledImage ("resources/pirate-ship.png", 40, 40);
+		shipImage.setSize(40, 40);
+		gamePanel.add(shipImage, Integer.valueOf(100));
+		
+		Random randomGenerator = new Random();
+		int pirateX = randomGenerator.nextInt(735);
+		int pirateY = randomGenerator.nextInt(360);
+		shipImage.setLocation(pirateX, pirateY);
 
 		// Add the panel to the frame
 		frame.setContentPane(contentPane);
